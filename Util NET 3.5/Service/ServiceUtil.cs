f6efbaac6ac325ae5fb714da29e93eab3f11f9d4
gavin <709181180@qq.com>
2015-06-18 14:47:31 +0800

    ServiceUtil refactored

--- Util NET 3.5/Service/ServiceUtil.cs
+++ Util NET 3.5/Service/ServiceUtil.cs
@@ -34,40 +34,6 @@ namespace Util.Service
             ManagedInstallerClass.InstallHelper(new[] { filepath });
         }
 
-        /// <summary>
-        /// 获取指定服务.exe程序的服务名; 失败返回String.Empty
-        /// </summary>
-        /// <param name="filepath">.exe程序路径</param>
-        /// <returns>服务名</returns>
-        public static String GetServiceName(String filepath)
-        {
-            try
-            {
-                Assembly assembly = Assembly.LoadFile(filepath);
-
-                foreach (var type in assembly.GetTypes())
-                {
-                    if (type.BaseType != typeof(Installer)) continue;
-
-                    //创建一个安装实例
-                    var installer = ((Installer)Activator.CreateInstance(type));
-
-                    //遍历该类型所有实例成员的非公开属性
-                    foreach (var item in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
-                    {
-                        if (item.FieldType != typeof(ServiceInstaller)) continue;
-
-                        //获取安装实例中ServiceInstaller成员的值
-                        var serviceInstaller = (ServiceInstaller)item.GetValue(installer);
-                        return serviceInstaller.ServiceName;
-                    }
-                }
-            }
-            catch (Exception) { }
-
-            return String.Empty;
-        }
-
         #endregion
 
         #region 卸载
@@ -79,87 +45,6 @@ namespace Util.Service
         public static void UnInstall(String filepath)
         {
             ManagedInstallerClass.InstallHelper(new[] { "/u", filepath });
-        }
-
-        #endregion
-
-        #region 状态
-
-        /// <summary>
-        /// 检查服务存在的存在性
-        /// </summary>
-        /// <param name="serviceName">服务名</param>
-        /// <returns>存在返回true,否则返回false;</returns>
-        public static Boolean Exists(String serviceName)
-        {
-            //遍历系统服务
-            foreach (ServiceController controller in ServiceController.GetServices())
-            {
-                if (controller.ServiceName.EqualsEx(serviceName))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// 服务是否已经停止
-        /// </summary>
-        /// <param name="serviceName">服务名</param>
-        /// <returns>已停止true;否则false</returns>
-        public static Boolean IsStopped(String serviceName)
-        {
-            ServiceController controller = new ServiceController(serviceName);
-
-            try
-            {
-                if (controller.Status.Equals(ServiceControllerStatus.Stopped))
-                {
-                    return true;
-                }
-
-                return false;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// 服务是否正在运行
-        /// </summary>
-        /// <param name="serviceName">服务名</param>
-        /// <returns>正在运行true;否则false</returns>
-        public static Boolean IsRunning(String serviceName)
-        {
-            ServiceController controller = new ServiceController(serviceName);
-
-            try
-            {
-                if (controller.Status.Equals(ServiceControllerStatus.Running))
-                {
-                    return true;
-                }
-
-                return false;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-
-        /// <summary>
-        /// 获取服务状态枚举ServiceControllerStatus
-        /// </summary>
-        /// <param name="serviceName">服务名</param>
-        /// <returns>ServiceControllerStatus状态枚举</returns>
-        public static ServiceControllerStatus GetServiceStatus(String serviceName)
-        {
-            return new ServiceController(serviceName).Status;
         }
 
         #endregion
@@ -222,6 +107,147 @@ namespace Util.Service
             }
 
             return false;
+        }
+
+        #endregion
+
+        #region 辅助
+
+        /// <summary>
+        /// 获取当前路径下的第一个服务应用程序
+        /// </summary>
+        /// <returns>当前路径下的第一个服务应用程序 或者 String.Empty</returns>
+        public static String GetCurrentServiceEXE()
+        {
+            foreach (var item in Directory.GetFiles(Environment.CurrentDirectory, "*.exe"))
+            {
+                Assembly assembly = Assembly.LoadFile(item);
+
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (type.BaseType == typeof(Installer)) return item;
+                }
+            }
+
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// 获取指定服务.exe程序的服务名; 失败返回String.Empty
+        /// </summary>
+        /// <param name="filepath">.exe程序路径</param>
+        /// <returns>服务名</returns>
+        public static String GetServiceName(String filepath)
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFile(filepath);
+
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (type.BaseType != typeof(Installer)) continue;
+
+                    //创建一个安装实例
+                    var installer = ((Installer)Activator.CreateInstance(type));
+
+                    //遍历该类型所有实例成员的非公开属性
+                    foreach (var item in type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic))
+                    {
+                        if (item.FieldType != typeof(ServiceInstaller)) continue;
+
+                        //获取安装实例中ServiceInstaller成员的值
+                        var serviceInstaller = (ServiceInstaller)item.GetValue(installer);
+                        return serviceInstaller.ServiceName;
+                    }
+                }
+            }
+            catch (Exception) { }
+
+            return String.Empty;
+        }
+
+        /// <summary>
+        /// 检查服务存在的存在性
+        /// </summary>
+        /// <param name="serviceName">服务名</param>
+        /// <returns>存在返回true,否则返回false;</returns>
+        public static Boolean Exists(String serviceName)
+        {
+            //遍历系统服务
+            foreach (ServiceController controller in ServiceController.GetServices())
+            {
+                if (controller.ServiceName.Equals(serviceName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 服务是否已经停止
+        /// </summary>
+        /// <param name="serviceName">服务名</param>
+        /// <returns>已停止true;否则false</returns>
+        public static Boolean IsStopped(String serviceName)
+        {
+            try
+            {
+                ServiceController controller = new ServiceController(serviceName);
+
+                if (controller.Status.Equals(ServiceControllerStatus.Stopped))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 服务是否正在运行
+        /// </summary>
+        /// <param name="serviceName">服务名</param>
+        /// <returns>正在运行true;否则false</returns>
+        public static Boolean IsRunning(String serviceName)
+        {
+            try
+            {
+                ServiceController controller = new ServiceController(serviceName);
+
+                if (controller.Status.Equals(ServiceControllerStatus.Running) || controller.Status.Equals(ServiceControllerStatus.StartPending))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 获取服务状态枚举ServiceControllerStatus
+        /// </summary>
+        /// <param name="serviceName">服务名</param>
+        /// <returns>ServiceControllerStatus状态枚举</returns>
+        public static ServiceControllerStatus GetServiceStatus(String serviceName)
+        {
+            try
+            {
+                return new ServiceController(serviceName).Status;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         #endregion
